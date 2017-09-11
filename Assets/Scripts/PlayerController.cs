b8c7a712b4d529d786a4c5f3ca13bd69252b7a87
owen <thss15_zhangqr@163.com>
2017-09-11 17:38:39 +0800

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class PlayerController : MonoBehaviour {
+
+	public int speed;
+	public Text countText;
+	public Text winText;
+
+	private int count;
+	private Rigidbody2D rb2d;
+	// Use this for initialization
+	void Start () 
+	{
+		rb2d = GetComponent<Rigidbody2D> ();
+		count = 0;
+		winText.text = "";
+		setCountText ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+		if (count >= 8) 
+		{
+			winText.text = "You Win";
+		}
+
+	}
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.CompareTag ("PickUp")) 
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			setCountText ();
+		}
+	}
+	void setCountText()
+	{
+		countText.text = "Count:" + count.ToString();
+	}
+}
